ba30093434c72621067cb1c5156a4b2fc1e7d47e
Bohdan Karashchuk <70644164+eternityduck@users.noreply.github.com>
2022-07-16 11:47:39 +0300

    Just a comment

--- GavAnimeAPI/Constant/Constants.cs
+++ GavAnimeAPI/Constant/Constants.cs
@@ -3,6 +3,7 @@
 public class Constants
 {
     public static string baseAddress = "https://kitsu.io/api/edge/";
+    //You cannot store the secret keys in the code, for such logic you need to use at least app.settings.json file excluded in .gitignore
     public static string AccessKey = "AKIA2D23RH7DWILQ3UXY";
     public static string SecretKey = "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";
 
