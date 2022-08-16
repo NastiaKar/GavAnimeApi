43699f5e49df47d48550c518a0c2c4b0120e2c06
NastiaKar <nastia.karpyuk@outlook.com>
2022-08-16 23:20:23 +0300

    added environment variables

--- GavAnimeAPI/Constant/Constants.cs
+++ GavAnimeAPI/Constant/Constants.cs
@@ -3,8 +3,8 @@
 public class Constants
 {
     public static string baseAddress = "https://kitsu.io/api/edge/";
-    public static string AccessKey = "AKIA2D23RH7DWILQ3UXY";
-    public static string SecretKey = "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";
+    public static string? AccessKey = Environment.GetEnvironmentVariable("AccessKey", EnvironmentVariableTarget.Machine);
+    public static string? SecretKey = Environment.GetEnvironmentVariable("SecretKey", EnvironmentVariableTarget.Machine);
 
     public static string TableName = "FavoriteAnime";
 }
